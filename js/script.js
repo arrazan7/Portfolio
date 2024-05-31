72171454f990073f37e30463613978ff19c021c7
arrazan7 <fayyadharrazanmiftakhul@mail.ugm.ac.id>
2024-06-01 02:24:27 +0700

    [01/06/2024_02:25/WIB]

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const nav = document.getElementById('nav');
+
+window.addEventListener('scroll', function() {
+    scrollPosition = window.scrollY;
+
+    if (scrollPosition >= 60) {
+        nav.classList.add('nav-dark');
+    }
+    else if (scrollPosition <= 60) {
+        nav.classList.remove('nav-dark');
+    }
+})
